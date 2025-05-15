f8b586ec8a28bc5f8c1c72ed58600e445477d0e7
NetoA <ernestoaguirreco@hotmail.com>
2025-05-15 15:43:22 -0600

    Revert "cinco copias"
    This reverts commit 0093e9746b6a440cee899ff970164f1a05bd7912.

--- Program.cs
+++ Program.cs
@@ -1,8 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hola, esto muestra una nueva modifocación!");
-Console.WriteLine("Hola, esto muestra una nueva modifocación!");
-Console.WriteLine("Hola, esto muestra una nueva modifocación!");
-Console.WriteLine("Hola, esto muestra una nueva modifocación!");
-Console.WriteLine("Hola, esto muestra una nueva modifocación!");
-
 
